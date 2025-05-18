c372615a42d9a5fc90efd857bd8bb69df6f9c5bb
Muhammadharis786 <144783788+Muhammadharis786@users.noreply.github.com>
2025-05-18 11:30:10 +0500

    Update content.jsx

--- src/content.jsx
+++ src/content.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Content = () => {
   return (
     <main style={styles.mainContent}>
-      <h2>Welcome to the Maju Dashboards</h2>
+      <h2>Welcome Dashboards</h2>
       <p>Here is your content area. You can display charts, data, or anything you like.</p>
     </main>
   );
